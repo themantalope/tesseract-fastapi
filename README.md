342f0183628de1571f6380730f3cdcaf33c536a5
themantalope <matthew.antalek@gmail.com>
2023-05-06 19:17:38 -0500

    comments/docs

--- README.md
+++ README.md
@@ -2,15 +2,26 @@
 
 little project to set up a tesseract-ocr server with fastapi
 
-## enpoints
+## endpoints
 
-### /ocr
+### POST
 
-- method: POST
+- **Description**: Process multiple image files and extract text using OCR.
+- **Parameters**:
+  - `files` (form-data): List of image files to be uploaded.
+- **Returns**:
+  - A list of dictionaries containing the filename and the extracted text for each image.
 
-  - body: multipart/form-data
-    - image: file
+### GET
 
-- method: GET
-  - params:
-    - url: str
+- **Description**: Process multiple image URLs and extract text using OCR.
+- **Parameters**:
+  - `urls` (query): List of image URLs to be processed.
+- **Returns**:
+  - A list of dictionaries containing the URL and the extracted text for each image. If an error occurs while fetching an image, an error message is included instead of the text.
+
+## Usage
+
+1. Build the Docker image: `docker build -t ocr-app .`
+2. Run the Docker container: `docker run -p 8000:8000 ocr-app`
+3. Access the OCR API at `http://localhost:8000/ocr`.
--- app.py
+++ app.py
@@ -6,22 +6,46 @@ from pytesseract import image_to_string
 import requests
 from io import BytesIO
 from PIL import Image
+from typing import List
 
 app = FastAPI()
 
 @app.post("/ocr")
-async def ocr_image(file: UploadFile = File(...)):
-    image = Image.open(file.file)
-    text = image_to_string(image)
-    return {"text": text}
+async def ocr_image(files: List[UploadFile] = File(...)):
+    """
+    Endpoint: /ocr (POST)
+    Description: Process multiple image files and extract text using OCR.
+    Parameters:
+        - files: List of image files to be uploaded.
+    Returns:
+        - A list of dictionaries containing the filename and the extracted text for each image.
+    """
+    results = []
+    for file in files:
+        image = Image.open(file.file)
+        text = image_to_string(image)
+        results.append({"filename": file.filename, "text": text})
+    return results
 
 @app.get("/ocr")
-async def ocr_image_url(url: str = Query(...)):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        image = Image.open(BytesIO(response.content))
-        text = image_to_string(image)
-        return {"text": text}
-    except requests.exceptions.RequestException as e:
-        return JSONResponse(content={"error": "Failed to fetch the image from the provided URL."}, status_code=400)
+async def ocr_image_urls(urls: List[str] = Query(...)):
+    """
+    Endpoint: /ocr (GET)
+    Description: Process multiple image URLs and extract text using OCR.
+    Parameters:
+        - urls: List of image URLs to be processed.
+    Returns:
+        - A list of dictionaries containing the URL and the extracted text for each image.
+          If an error occurs while fetching an image, an error message is included instead of the text.
+    """
+    results = []
+    for url in urls:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            image = Image.open(BytesIO(response.content))
+            text = image_to_string(image)
+            results.append({"url": url, "text": text})
+        except requests.exceptions.RequestException as e:
+            results.append({"url": url, "error": "Failed to fetch the image from the provided URL."})
+    return results
